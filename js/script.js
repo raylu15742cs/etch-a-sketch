e3d84e63d716207dd73a33e542dc778c9ccda209
raylu15742cs <raylu15742cs@gmail.com>
2022-04-26 16:56:22 -0700

    add eventlistener and color change

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 const box_container = document.querySelector('.box_container')
 
 let createbox = function(height) {
-    heights = 700/ height;
-    heights += 'px';
+    heights = (700/height) + 'px';
     for (let i = 1; i <= height; i++) {
         box = document.createElement('div');
         box.classList.add(i);
@@ -17,4 +16,11 @@ let createbox = function(height) {
         }
     }
 }
-createbox(4);
+createbox(3);
+
+const listens = document.querySelectorAll(".border")
+listens.forEach(listen =>  {
+    listen.addEventListener('mouseover', function() {
+        listen.style.backgroundColor = 'black';
+    })
+})
